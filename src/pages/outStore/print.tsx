046084aaf3ad601226f8aed71c2d10185a4d5df4
黄丙璋 <huangbingzhang@lazbao.com>
2024-11-05 22:36:46 +0800

    style

--- src/pages/outStore/print.tsx
+++ src/pages/outStore/print.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useRef, useEffect } from 'react'
-import { SearchBar, Toast, Button } from 'antd-mobile'
+import { SearchBar, Toast, Button, SpinLoading } from 'antd-mobile'
 import { ScanningOutline } from 'antd-mobile-icons';
 import { getRealStr } from '@/utils'
 import { printPDF } from '@/server/print';
@@ -9,20 +9,22 @@ import styles from './index.less'
 const Print = () => {
   const [orderName, setOrderName] = useState<any>(null)
   const searchRef = useRef<any>(null)
+  const [loading, setLoading] = useState<boolean>(false)
 
-  useEffect(() => {
-    // console.log(searchRef);
-    if(searchRef.current){
-      searchRef.current.focus()
-    }
-  },[searchRef])
+
+  // useEffect(() => {
+  //   // console.log(searchRef);
+  //   if(searchRef.current){
+  //     searchRef.current.focus()
+  //   }
+  // },[searchRef])
   const onFocusHandle = () => {
     window.wx.scanQRCode({
       needResult: 1, // 默认为0，扫描结果由微信处理，1则直接返回扫描结果，
       success: function(res: any) {
         const realStr = getRealStr(res.resultStr)
         setOrderName(realStr)
-        printHandle()
+        printHandle(realStr)
       },
       error: function(){
         Toast.show({
@@ -33,28 +35,29 @@ const Print = () => {
     })
   }
 
-  const printHandle = () => {
+  const printHandle = (code:any) => {
 
-    if(!orderName) {
+    if(!orderName && !code) {
       Toast.show({
         icon: 'fail',
         content: "请扫描单号",
       })
       return 
     }
-    printPDF({order_name: orderName}).then(res => {
+    printPDF({order_name: code || orderName}).then(res => {
       Toast.show({
         icon: 'success',
         content: '打印成功',
       })
     }).finally(()=>{
       setOrderName("")
-      searchRef?.current?.blur()
+      // searchRef?.current?.blur()
     })
   }
   return (
-    <div>
+    <div className={styles.content}>
       <SearchBar placeholder="请输入" value={orderName} onChange={setOrderName} ref={searchRef} onSearch={printHandle}  searchIcon={<ScanningOutline  onClick={onFocusHandle} />} clearable />
+      {loading && <SpinLoading  style={{ '--size': '48px' , margin: '240px auto'}} /> }
       <div className={styles.btmScan}>
         <Button size='large' color='primary' style={{"--border-radius": "50%"}} onClick={onFocusHandle}>
           <ScanningOutline />
